d016f9060d92b845f1fb3f9bf512d7de24343be8
scotej <134114466+scotej@users.noreply.github.com>
2026-05-08 14:11:37 +1000

    add import statement for codrone_edu and fix comment formatting in events.py

--- events.py
+++ events.py
@@ -1,8 +1,10 @@
 # Import and initialize the pygame library
 import pygame
+from codrone_edu import *
+
 pygame.init()
 
-# Set up the drawing window
+# Set up the drawing    window
 screen = pygame.display.set_mode([500, 500])
 
 # Initial position of the circle
